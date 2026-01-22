45ea2461b00ddb76f4f820402cd2e7806247a887
SanchTechInc <d.sanch.1105@gmail.com>
2026-01-22 03:21:47 -0800

    middelware.ts updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,16 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
     /*
-     * Match all routes except for:
-     * - static files
-     * - Next.js internals
+     * Match all request paths except:
+     * - _next (static files)
+     * - favicon
+     * - public files
      */
-    "/((?!_next|.*\\..*).*)",
+    "/((?!_next|favicon.ico).*)",
   ],
 };
